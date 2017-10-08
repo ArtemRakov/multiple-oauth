f3d77ab8b5be771e6972114244a0c298bbc5dfe1
Artem Rakov <scoff1k@gmail.com>
2017-10-08 17:48:41 +0100

    added multiple oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,32 +1,69 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :set_service
+  before_action :set_user
+  attr_reader :service, :user
+
   def facebook
-    service = Service.find_by(provider: auth.provider, uid: auth.uid)
+    handle_auth "facebook"
+  end
 
+  def twitter
+    handle_auth "twitter"
+  end
+
+  private
+
+  def handle_auth(kind)
     if service.present?
-      user = service.user
-      service.update(
-        expires_at: Time.at(auth.credentials.expires_at),
-        access_token: auth.credentials.token
-        )
+      service.update(service_attrs)
     else
-      user = User.create(
-        email: auth.info.email,
-        password: Devise.friendly_token[0, 20]
-      )
-
-      user.services.create(
-        provider: auth.provider,
-        uid: auth.uid,
-        expires_at: Time.at(auth.credentials.expires_at),
-        access_token: auth.credentials.token
-      )
+      user.services.create(service_attrs)
     end
 
-    sign_in_and_redirect user, event: :authetication
-    set_flash_message :notice, :success, kind: "Facebook"
+    if user_signed_in?
+      flash[:notice] = "Your #{kind} account was connected"
+      redirect_to edit_user_registration_path
+    else
+      sign_in_and_redirect user, event: :authetication
+      set_flash_message :notice, :success, kind: kind
+    end
   end
 
   def auth
     request.env['omniauth.auth']
+  end
+
+  def set_service
+    @service = Service.where(provider: auth.provider, uid: auth.uid).first
+  end
+
+  def set_user
+    if user_signed_in?
+      @user = current_user
+    elsif service.present?
+      @user = service.user
+    elsif User.where(email: auth.info.email).any?
+        flash[:alert] = "An account with this email already exist. Please sign in with this account before connecting your #{auth.provider.titleize} account"
+        redirect_to new_user_session_path
+    else
+      @user = User.create(create_user)
+    end
+  end
+
+  def service_attrs
+    expires_at = auth.credentials.expires_at.present? ? Time.at(auth.credentials.expires_at) : nil
+    {
+      provider: auth.provider,
+      uid: auth.uid,
+      expires_at: expires_at,
+      access_token: auth.credentials.token
+    }
+  end
+
+  def create_user
+    {
+      email: auth.info.email,
+      password: Devise.friendly_token[0, 20]
+    }
   end
 end
